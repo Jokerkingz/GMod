f6cc81d56b1caf67a89077eac8165f320b0f6c91
Abe <abers1997@gmail.com>
2017-11-28 20:17:09 -0500

    Abe's scene updated

---
+++ Assets/Abe/PrintingBenchPoop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintingBenchPoop : MonoBehaviour {
+
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+	void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            anim.Play("Poop");
+        }
+	}
+}
