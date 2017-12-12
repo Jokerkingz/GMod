e43218b987a15ecd69f49b7163bb84e9e3a5e8e9
Unknown <ChristLaurenzs@gmail.com>
2017-12-11 19:13:38 -0500

    mix in

--- Assets/Scripts/Scr_Belt.cs
+++ Assets/Scripts/Scr_Belt.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Scr_Belt : MonoBehaviour {
-
+	public GameObject vObj;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.eulerAngles = Vector3.Scale(transform.eulerAngles,new Vector3(0f,1f,0f));
+		transform.eulerAngles = new Vector3(0f,vObj.transform.eulerAngles.y,0f);
+		//transform.eulerAngles = Vector3.Scale(transform.eulerAngles,new Vector3(0f,1f,0f));
 	}
 }
--- Assets/Scripts/Scr_Scaler.cs
+++ Assets/Scripts/Scr_Scaler.cs
@@ -2,27 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scr_Rotator : MonoBehaviour {
-private int vAccAngle;
-private float vAngle;
+public class Scr_Scaler : MonoBehaviour {
+	private int vAccScale;
+private float vScale;
 	void Start(){
 		}
 	// Use this for initialization
 	void Update () {
 		//Triggered ();
-		if (vAccAngle > 0)
-			vAccAngle --;
+		if (vScale > 0)
+			vScale -= Time.deltaTime*5f;
+
+		vScale += vAccScale*.1f;
+		vScale = Mathf.Clamp(vScale,.5f,7.5f);
+		this.transform.localScale = new Vector3(vScale,vScale,vScale);
 	}
 	
 	// Update is called once per frame
 	public void Triggered (){
-		this.transform.localEulerAngles = new Vector3(0f,vAngle,0f);
-		if (vAccAngle < 45)
-		vAccAngle += 3;
-
-		vAngle += vAccAngle*.3f;
-		if (vAngle > 360)
-			vAngle -= 360;
+		if (vScale < 7.5)
+			vScale += 50f*Time.deltaTime;
 		//cRB.angularVelocity = new Vector3(0f,50f,0f);
 		//transform.Rotate(new Vector3(0f,5f,0f));
 	}
