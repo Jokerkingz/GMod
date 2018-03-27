883f720794faf7c93d49d8e923bf2eb0ade5a87f
Unknown <ChristLaurenzs@gmail.com>
2018-03-27 19:23:08 -0400

    Rail gun addition

---
+++ Assets/Scripts/Rework/Scr_DestroyTime.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_DestroyTime : MonoBehaviour {
+	// Use this for initialization
+	public void fStartTimer (float tTimer) {
+		Invoke("DestroyThis",tTimer);
+	}
+	
+	// Update is called once per frame
+	void DestroyThis() {
+		Destroy(this.gameObject);
+	}
+}
