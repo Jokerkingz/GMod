540ae323fe8f5b53fbcff2c6b778857372798e07
Dylan Rosenthal <dylan2k6@hotmail.com>
2018-03-29 15:25:40 -0400

    Gravity Grenade/FPS Controller/GravParticle

---
+++ Assets/Dylan/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public float speed = 6.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	public float horizontalSpeed = 3.0f;
+
+	void Update () 
+	{
+		CharacterController controller = GetComponent<CharacterController> ();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("Jump"))
+				moveDirection.y = jumpSpeed;
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+		float h = horizontalSpeed * Input.GetAxis ("RightJoystickHorizontal");
+		transform.Rotate (0, h, 0);
+	}
+}
