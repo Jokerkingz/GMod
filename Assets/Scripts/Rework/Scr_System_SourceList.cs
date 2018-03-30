c19583e8e38cf145cc806fb168efdc2741c2256a
Unknown <ChristLaurenzs@gmail.com>
2018-03-30 13:47:34 -0400

    Before the Fan Fixed

---
+++ Assets/Scripts/Rework/Scr_System_SourceList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_System_SourceList : MonoBehaviour {
+	[System.Serializable]
+	public class Parts
+	{	public string vName;
+		public GameObject vSource;
+		public string vMainCategory;
+		public string vModName;
+		public string vModType;
+
+	}
+	public Parts[] PartList;
+
+	public GameObject fGetPrefab(string name){
+
+
+
+
+	}
+}
