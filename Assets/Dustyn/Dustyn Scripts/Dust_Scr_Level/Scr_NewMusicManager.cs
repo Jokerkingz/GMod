055ef46a06f0f6c219e5e4a292aebed10ace460c
Unknown <dustyn.alberts@hotmail.com>
2018-04-03 15:44:10 -0400

    Update (Fixes, musicmanager scr, sound correction)

---
+++ Assets/Dustyn/Dustyn Scripts/Dust_Scr_Level/Scr_NewMusicManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_NewMusicManager : MonoBehaviour {
+
+	[Header("References")]
+	public GameObject passiveMusic;
+	public GameObject combatMusic;
+	public GameObject transitionSound;
+	public GameObject finalRoomMusicIntro;
+	public GameObject finalRoomMusicLoop;
+	[Header("Floats")]
+	public float fadeTime;
+	public float finalRoomIntroTimer;
+	[Header("Bools")]
+	public bool crossFadeIn;
+	public bool crossFadeOut;
+	void Start () {
+	
+
+	}
+	
+	void Update()
+	{
+
+		//------TESTING
+	if (Input.GetKeyDown(KeyCode.A))
+	{
+		PlayCombatMusic();
+	}
+	if (Input.GetKeyDown(KeyCode.S))
+	{
+		PlayPassiveMusic();
+	}
+	if (Input.GetKeyDown(KeyCode.W))
+	{
+		PlayFinalRoomMusicIntro();
+	}
+
+
+		if (crossFadeIn)
+		{
+			passiveMusic.GetComponent<AudioSource>().volume = passiveMusic.GetComponent<AudioSource>().volume-fadeTime;
+			combatMusic.GetComponent<AudioSource>().volume = combatMusic.GetComponent<AudioSource>().volume +fadeTime;
+
+			if (passiveMusic.GetComponent<AudioSource>().volume<=0 && combatMusic.GetComponent<AudioSource>().volume>=1)
+			{
+				crossFadeIn=false;
+			}
+		}
+
+		if (crossFadeOut)
+		{
+			passiveMusic.GetComponent<AudioSource>().volume=passiveMusic.GetComponent<AudioSource>().volume +fadeTime;
+			combatMusic.GetComponent<AudioSource>().volume =combatMusic.GetComponent<AudioSource>().volume-fadeTime;
+
+			if (passiveMusic.GetComponent<AudioSource>().volume>=1 && combatMusic.GetComponent<AudioSource>().volume<=0)
+			{
+				crossFadeOut=false;
+			}
+		}
+
+		if (passiveMusic.GetComponent<AudioSource>().volume>=1)
+		{passiveMusic.GetComponent<AudioSource>().volume=1;}
+		if (passiveMusic.GetComponent<AudioSource>().volume<=0)
+		{passiveMusic.GetComponent<AudioSource>().volume=0;}
+
+		if (combatMusic.GetComponent<AudioSource>().volume>=1)
+		{combatMusic.GetComponent<AudioSource>().volume=1;}
+		if (combatMusic.GetComponent<AudioSource>().volume<=0)
+		{combatMusic.GetComponent<AudioSource>().volume=0;}
+
+	}
+	void PlayCombatMusic()
+	{
+		crossFadeIn=true;
+		transitionSound.GetComponent<AudioSource>().Play();
+		Debug.Log("StartCombatMusic");
+	}
+	void PlayPassiveMusic()
+	{
+		crossFadeOut =true;
+		transitionSound.GetComponent<AudioSource>().Play();
+		Debug.Log("Start Passive Music");
+	}
+
+	void PlayFinalRoomMusicIntro()
+	{
+		finalRoomMusicIntro.GetComponent<AudioSource>().Play();
+		Debug.Log("Playing Final Intro");
+		StartCoroutine (WaitForIntroToEnd());
+	}
+
+	IEnumerator WaitForIntroToEnd()
+	{
+		yield return new WaitForSeconds(finalRoomIntroTimer);
+		PlayFinalRoomMusicRoomLoop();
+	}
+
+	void PlayFinalRoomMusicRoomLoop()
+	{
+		finalRoomMusicLoop.GetComponent<AudioSource>().Play();
+		Debug.Log("Playing Final Loop");
+	}
+}
---
+++ Assets/Dustyn/Dustyn Scripts/Dust_Scr_Level/Scr_musicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_musicManager : MonoBehaviour {
+
+	public GameObject firstLayer;
+	public GameObject secondLayer;
+	public bool secondLayerFadeIn;
+	public bool secondLayerFadeOut;
+	void Start () {
+		
+		firstLayer.GetComponent<AudioSource>().Play();
+		secondLayer.GetComponent<AudioSource>().volume=0;
+	}
+	
+	void Update () {
+		if (secondLayerFadeIn)
+		{
+			secondLayer.GetComponent<AudioSource>().volume =secondLayer.GetComponent<AudioSource>().volume +0.01f;
+		}
+
+		if (secondLayer.GetComponent<AudioSource>().volume ==1)
+		{
+			secondLayerFadeIn=false;
+		}
+	}
+
+	public void PlaySecondLayer()
+	{
+		secondLayerFadeIn=true;
+	}
+}
