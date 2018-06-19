6c2adf8b598aecf5701bc8a63672e14b957599be
Dylan Rosenthal <dylan2k6@hotmail.com>
2018-06-19 11:35:04 -0400

    Asteroid Rotation + Credit Values + New Grid Texture

---
+++ Assets/Dylan/YRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YRotate : MonoBehaviour {
+
+    public float RotationSpeed = 2f;
+
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * RotationSpeed, Space.World);
+	}
+}
