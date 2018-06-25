67779c493a26b5c1dbe8e311a0734b192a9de565
Dylan Rosenthal <dylan2k6@hotmail.com>
2018-06-25 17:40:33 -0400

    NewScripts folder (SpawnWave, ScoreCount)

---
+++ Assets/Dylan/NewScripts/GetComp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetComp : MonoBehaviour {
+    private ScoreCount kill;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject theScore = GameObject.Find("ScoreCounter");
+        ScoreCount scoreBoard = theScore.GetComponent<ScoreCount>();
+        if (Input.GetKeyDown("space"))
+        {
+            scoreBoard.score += 10;
+
+        }
+	}
+}
---
+++ Assets/Dylan/NewScripts/ScoreCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour {
+
+    public Text MyText;
+    public int score;
+
+    void Start()
+    {
+
+        MyText.text = "";
+
+    }
+
+
+    void Update()
+    {
+
+        MyText.text = "" + score;
+
+
+    }
+    void AddPoints(){
+        score = score + 1;
+
+    }
+
+}
